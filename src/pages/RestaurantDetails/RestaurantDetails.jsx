114627510e3e66e8aeab118de9c7ff9924e2e122
Nani1345 <xuna1026000@email.com>
2024-09-07 11:39:50 -0700

    creates route for restaurant details in app.jsx

---
+++ src/pages/RestaurantDetails/RestaurantDetails.jsx
@@ -0,0 +1,12 @@
+// css
+import styles from './RestaurantDetails.module.css'
+
+const RestaurantDetails = () => {
+  return (
+    <main className={styles.container}>
+      Details
+    </main>
+  )
+}
+
+export default RestaurantDetails
