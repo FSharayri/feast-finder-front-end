45aa23d82b78cfd8c6fdb919a375c6a25ef61cd8
Ito Aghayere <itohan.aghayere@gmail.com>
2024-09-06 18:15:02 -0700

    stubs up dish list and restaurant list pages

---
+++ src/pages/DishList/DishList.jsx
@@ -0,0 +1,11 @@
+// css
+import styles from './RestaurantList.module.css'
+
+// import DishCard from '../../components/DishCard/DishCard'
+
+const DishList = () => {
+
+  }
+  
+
+export default DishList
