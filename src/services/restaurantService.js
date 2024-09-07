0487d96ed261c47e9bd413648e9e90aadf5d91e5
Nani1345 <xuna1026000@email.com>
2024-09-06 18:52:39 -0700

    implements restaurant index function in restaurant service

---
+++ src/services/restaurantService.js
@@ -0,0 +1,18 @@
+// services
+import * as tokenService from './tokenService'
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/restaurants`
+
+async function index() {
+  try {
+    const res = await fetch(BASE_URL)
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+export {
+    index,
+}
