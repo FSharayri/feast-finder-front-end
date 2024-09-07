6e4bef79f77c5e872d60481b9323b0ea03bf80dc
Nani1345 <xuna1026000@email.com>
2024-09-07 13:00:30 -0700

    Roughly implements restaurant details function in restautant details jsx

--- src/pages/RestaurantDetails/RestaurantDetails.jsx
+++ src/pages/RestaurantDetails/RestaurantDetails.jsx
@@ -2,23 +2,39 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 
+// services
+import * as restaurantService from '../../services/restaurantService'
+
 // css
 import styles from './RestaurantDetails.module.css'
 
-const RestaurantDetails = (props) => {
+const RestaurantDetails = () => {
   const {restaurantId} = useParams()
-  const [restaurant, setRestaurant] = useState (null)
+  const [restaurant, setRestaurant] = useState(null)
 
   useEffect(() => {
     const fetchRestaurant = async () => {
-      // const blogData <-- We'll put the 'show' service function here
+      const restaurantData = await restaurantService.show(restaurantId)
       setRestaurant(restaurantData)
     }
     fetchRestaurant()
   }, [restaurantId])
+  
+  if(!restaurant) return <p>Loading...</p>
+  
   return (
     <main className={styles.container}>
-      Details
+      <article>
+        <header>
+        <h1>{restaurant.logo}</h1>
+        <h1>{restaurant.name}</h1>
+        <h3>{restaurant.cuisine}</h3>
+        <h3>{restaurant.zipcode}</h3>
+        </header>
+      </article>
+      <section>
+        <h1>Dishes list</h1>
+      </section>
     </main>
   )
 }
--- src/services/restaurantService.js
+++ src/services/restaurantService.js
@@ -1,5 +1,5 @@
 // services
-import * as tokenService from './tokenService'
+//import * as tokenService from './tokenService'
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/restaurants`
 
