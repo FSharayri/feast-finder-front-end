edef9386f596f508f623c3c639d5f38fcd448042
Ito Aghayere <itohan.aghayere@gmail.com>
2024-09-09 18:46:32 -0700

    fixes nav bar and explorer bar layout and adds icon in navbar linking to landing page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,27 +8,22 @@ import logo from '../../assets/branding/forker-nobgpng.png'
 import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
-
-  const publicLinks = (
-    <ul>
-      <li><NavLink to="/auth/login"><i className="fa-solid fa-right-to-bracket"></i> Log In</NavLink></li>
-      <li><NavLink to="/auth/signup"><i className="fa-solid fa-pen-to-square"></i> Sign Up</NavLink></li> 
-    </ul>
-  )
-
-  const protectedLinks = (
-    <ul>
-      <li>Welcome, {user.name}</li>  
-      <li><NavLink to="/profiles">Profiles</NavLink></li>
-      <li><NavLink to="" onClick={handleLogout}>Log Out <i className="fa-solid fa-right-from-bracket"></i></NavLink></li>
-      <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-    </ul>
-  )
-
   return (
     <nav className={styles.container}>
-      <NavLink to="/"><img src={logo} alt="A cute googly eyed fork" /></NavLink>
-      {user ? protectedLinks : publicLinks}
+       <NavLink to="/"><img src={logo} alt="A cute googly eyed fork" /></NavLink>
+      {user ?
+        <ul>
+          <li>Welcome, {user.name}</li>  
+          <li><NavLink to="/profiles">Profiles</NavLink></li>
+          <li><NavLink to="" onClick={handleLogout}>Log Out <i className="fa-solid fa-right-from-bracket"></i></NavLink></li>
+          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
+        </ul>
+      :
+        <ul>
+          <li><NavLink to="/auth/login"><i className="fa-solid fa-right-to-bracket"></i> Log In</NavLink></li>
+          <li><NavLink to="/auth/signup"><i className="fa-solid fa-pen-to-square"></i> Sign Up</NavLink></li> 
+        </ul>
+      }
     </nav>
   )
 }
