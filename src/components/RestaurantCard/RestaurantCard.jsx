3b9e1109048e14ac1aeb441ad097b2107428c273
Ito Aghayere <itohan.aghayere@gmail.com>
2024-09-11 16:39:58 -0700

    adds styling of restaurant card and other items

--- src/components/RestaurantCard/RestaurantCard.jsx
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -1,32 +1,27 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 
-// components
-
 // css
 import styles from './RestaurantCard.module.css'
 
 const RestaurantCard = ({ restaurant }) => {
   return (
-    <NavLink to={`/restaurants/${restaurant._id}`}>
-      {restaurant.photo 
-        ? <img
-            style={{ width: '320px' }}
-            src={restaurant.photo}
-            alt={`Photo of ${restaurant.name}`}
-          />
-        : <img src="https://picsum.photos/320/240/" alt="A random lorem picsum photo" />
-      }
-      <article className={styles.container}>
-        <header>
-          <span>
-            <h1>{restaurant.name}</h1>
-            <h1>{restaurant.cuisine}</h1>
-          </span>
-        </header>
-      </article>
-    </NavLink>
+    <article className={styles.container}>
+      <NavLink to={`/restaurants/${restaurant._id}`}>
+        {restaurant.photo 
+          ? <img
+              src={restaurant.photo}
+              alt={`Photo of ${restaurant.name}`}
+            />
+          : <img src="https://picsum.photos/320/240/" alt="A random lorem picsum photo" />
+        }
+        <div className={styles['text-container']}>
+          <h1 className={styles.name}>{restaurant.name}</h1>
+          <h2 className={styles.cuisine}>{restaurant.cuisine}</h2>
+        </div>
+      </NavLink>
+    </article>
   )
 }
 
-export default RestaurantCard
+export default RestaurantCard;
