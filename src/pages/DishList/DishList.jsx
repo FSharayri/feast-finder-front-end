40c0cd7e2c076833ce1277b101925bc3dee185b2
Faris <farissharayri@gmail.com>
2024-09-30 23:32:13 -0400

    completes cuisine filtering mechanism

--- src/pages/DishList/DishList.jsx
+++ src/pages/DishList/DishList.jsx
@@ -6,6 +6,7 @@ import styles from './DishList.module.css'
 
 //components 
 import DishCard from '../../components/DishCard/DishCard';
+import CuisineTabBar from '../../components/CuisineTabBar/CuisineTabBar'
 
 const DishList = ({dishes}) => {
   const [selectedCuisine , setSelectedCuisine] = useState(null);
@@ -21,12 +22,32 @@ const DishList = ({dishes}) => {
     for (let cuisine in cuisines){
       cuisinesArray.push([cuisine, cuisines[cuisine]])
     }
+  cuisinesArray.sort((a,b)=> b[1].length-a[1].length)
+  const cuisinesNames = cuisinesArray.map(cuisine=>cuisine[0])
+  const filteredCuisine = cuisinesArray?.filter(cuisine => cuisine[0]===selectedCuisine ) 
   return ( 
+    <><CuisineTabBar
+        cuisines={cuisinesNames}
+        selectedCuisine={selectedCuisine}
+        onSelectCuisine={setSelectedCuisine}
+    />
     <main className={styles.container}>
       {dishes.map(dish=>
         <DishCard dish={dish} key={dish._id}/>
       )}
+      {selectedCuisine?
+      filteredCuisine?.at(0).at(1).map(dish=>
+        <DishCard dish={dish} key={dish._id}/>
+      )
+      :
+      cuisinesArray.map(cuisine=>
+        cuisine[1].map(dish=>
+          <DishCard dish={dish} key={dish._id}/>
+        )
+      )
+      }
     </main>
+    </>
   );
 }
 
